edf25e42f7aab27141d36108928181052b62ee2c
Claes Heise <sadclat@gmail.com>
2025-11-06 18:50:38 +0100

    Fixed for mobile

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3091175728"
+    serviceWorkerVersion: "1602567517"
   }
 });
